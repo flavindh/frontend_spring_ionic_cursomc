001171f1e9c5e549e54f7c26b0b9d8da44c5d0a9
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-11-24 13:58:42 -0300

    Mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
 import { CategoriaService } from './../../services/domain/categoria.service';
+import { CategoriaDTO } from './../../models/categoria.dto';
 
 
 @IonicPage()
@@ -10,13 +12,15 @@ import { CategoriaService } from './../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  items: CategoriaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
     this.categoriaService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response;
       },
         error => {
           console.log(error);

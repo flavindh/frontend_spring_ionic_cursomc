73098a61bd76f754b097221a02163a06879f20c0
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-12-09 17:39:07 -0300

    Mostrando dados e imagem do cliente para pagina de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,8 @@
+import { ClienteDTO } from './../../models/cliente.dto';
 import { StorageService } from './../../services/storage.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
 @Component({
@@ -9,16 +11,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
   email: string;
+  cliente: ClienteDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService, public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
-      this.email = localUser.email;
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.cliente = response;
+        },
+          error => { });
     }
 
   }
-
+  
 }
